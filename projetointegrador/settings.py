5c5aa3a9936f9e97796b90ca39d204e0500a2cf1
Reinaldo <67565485+ReinaldoCFilho@users.noreply.github.com>
2023-09-15 18:23:55 -0300

    Implementação da Acessibilidade

--- projetointegrador/settings.py
+++ projetointegrador/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_simple_cookie_consent',
+    'accessibility',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'accessibility.context_processors.admin_AIOA',
             ],
         },
     },
