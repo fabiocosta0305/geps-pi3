f9a8d3e3ee6cffaf6aa7401d649bc56f18e3e788
Fábio Emilio Costa <fabiocosta0305@gmail.com>
2024-03-28 14:35:00 -0300

    Correção do settings.py

--- projetointegrador/settings.py
+++ projetointegrador/settings.py
@@ -96,19 +96,19 @@ WSGI_APPLICATION = 'projetointegrador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DATABASE_NAME'),
-#         'HOST': os.environ.get('DATABASE_HOST'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -121,16 +121,16 @@ WSGI_APPLICATION = 'projetointegrador.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'geps',
-        'HOST': '10.0.0.22',
-        'USER': 'geps',
-        'PASSWORD': 'geps',
-        'PORT': '' # padrão 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'geps',
+#         'HOST': '10.0.0.22',
+#         'USER': 'geps',
+#         'PASSWORD': 'geps',
+#         'PORT': '' # padrão 5432
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
